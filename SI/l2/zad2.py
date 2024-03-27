455a01f686a7f53585f3f9b6c6fcc92d147a4716
CzmeleN <344907@uwr.edu.pl>
2024-03-28 00:46:06 +0100

    Task 2 passing 8/11, almost there

--- SI/l2/zad2.py
+++ SI/l2/zad2.py
@@ -7,7 +7,7 @@ def make_moves(labirynth, poses, move):
     dy = 0
 
     if move == 'U':
-            dx = -1
+        dx = -1
     elif move == 'D':
         dx = 1
     elif move == 'L':
@@ -24,56 +24,94 @@ def make_moves(labirynth, poses, move):
             new_poses.append(new_pos)
 
     return new_poses
-    
+
+def get_move(dx, dy):
+    if dx == -1:
+        return 'U'
+    if dx == 1:
+        return 'D'
+    if dy == -1:
+        return 'L'
+    return 'R'
 
 def lower_uncertanity(labirynth, sizes):
-    moves = ['U', 'D', 'L', 'R']
-    min = float("inf")
+    # moves = ['U', 'D', 'L', 'R']
+    # min = float("inf")
     starting = []
     for i in range(sizes[0]):
             for j in range(sizes[1]):
                 if labirynth[i][j] == 'S' or labirynth[i][j] == 'B':
                     starting.append((i, j))
-    active = True
-    while(active):
-        path = ""
-        poses = starting.copy()
-        for j in range(110):
-            path += random.choice(moves)
-        for move in range(len(path)):
-            poses = make_moves(labirynth, poses, path[move], True)
-            if len(poses) < 2:
-                path = path[:move]
-                active = False
-                break
-    return (poses, path)
+    # active = True
+    # while(active):
+    #     path = ""
+    #     poses = starting.copy()
+    #     for j in range(80):
+    #         path += random.choice(moves)
+    #     for move in range(len(path)):
+    #         poses = make_moves(labirynth, poses, path[move])
+    #         if len(poses) == 3:
+    #             path = path[:move]
+    #             active = False
+    #             break
+    # print("done")
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    tuple_start = tuple(starting)
+    queue = deque([(tuple_start, "")])
+    visited = {tuple_start}
+
+    while(queue):
+        pos, path = queue.popleft()
+        lowest = len(pos)
+        
+        if lowest == 1:
+            break
+
+        for dx, dy in directions:
+            new_pos = []
+            for (x, y) in pos:
+                new_x, new_y = x + dx, y + dy
+                if labirynth[new_x][new_y] != '#':    
+                    if (new_x, new_y) not in new_pos:
+                        new_pos.append((new_x, new_y))
+                elif (x, y) not in new_pos:
+                    new_pos.append((x, y))
+            tuple_pos = tuple(new_pos)
+            if tuple_pos in visited:
+                continue
+            new_len = len(new_pos)
+            if new_len < lowest:
+                lowest = new_len
+                queue.clear()
+            queue.append((tuple_pos, path + get_move(dx, dy)))
+            visited.add(tuple_pos)
+
+    return (pos[0], path)
 
 def generate_path(labirynth, sizes):
-    starting = lower_uncertanity(labirynth, sizes)
-    print(starting)
-    moves = ['U', 'D', 'R', 'L']
-    visited = set()
-    visited.add(frozenset(starting[0]))
-    queue = deque()
-    queue.append(starting)
+    start_pos, start_path = lower_uncertanity(labirynth, sizes)
+    # print(start_path)
+    # return "RL"
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    tuple_start = tuple(start_pos)
+    queue = deque([(tuple_start, start_path)])
+    visited = {tuple_start}
 
-    while queue:
-        curr = queue.popleft()
-        visited.add(frozenset(curr[0]))
+    while(queue):
+        (x, y), path = queue.popleft()
+        curr = labirynth[x][y]
 
-        if all([labirynth[pos[0]][pos[1]] == 'B' for pos in curr[0]]):
-            # print(curr[0])
-            return curr[1]
-        cleared = False
-        for move in moves:
-            pos = make_moves(labirynth, curr[0], move, 0)
-            if cleared == False and len(pos[0]) < len(curr[0]):
-                queue.clear()
-                cleared = True
-            if frozenset(pos) not in visited:
-                # print(curr[1] + move)
-                queue.append((pos, curr[1] + move))
-    return -1        
+        if curr == 'G' or curr == 'B':
+            break
+        
+        for dx, dy in directions:
+            new_x, new_y = x + dx, y + dy
+            if labirynth[new_x][new_y] != '#':
+                if (new_x, new_y) not in visited:
+                    queue.append(((new_x, new_y), path + get_move(dx, dy)))
+                    visited.add((new_x, new_y))
+
+    return path
 
 if __name__ == "__main__":
     with open("zad_input.txt") as f:
