04a97275e66b6bc7d207a7e9f02f743d5ff2d0fe
CzmeleN <344907@uwr.edu.pl>
2024-03-28 01:47:05 +0100

    Tweaks for task 2, still not passing every test

--- SI/l2/zad2.py
+++ SI/l2/zad2.py
@@ -1,29 +1,14 @@
 from collections import deque
 import random
 
-def make_moves(labirynth, poses, move):
-    new_poses = []
-    dx = 0
-    dy = 0
-
-    if move == 'U':
-        dx = -1
-    elif move == 'D':
-        dx = 1
-    elif move == 'L':
-        dy = -1
-    else:
-        dy = 1
-
-    for i in range(len(poses)):
-        old_pos = poses[i]
-        new_pos = (old_pos[0] + dx, old_pos[1] + dy)
-        if labirynth[new_pos[0]][new_pos[1]] == '#':
-            new_pos = old_pos
-        if new_pos not in new_poses:
-            new_poses.append(new_pos)
-
-    return new_poses
+def get_dir(m):
+    if m == 'U':
+        return (-1, 0)
+    if m == 'D':
+        return (1, 0)
+    if m == 'L':
+        return (0, -1)
+    return (0, 1)
 
 def get_move(dx, dy):
     if dx == -1:
@@ -35,29 +20,40 @@ def get_move(dx, dy):
     return 'R'
 
 def lower_uncertanity(labirynth, sizes):
-    # moves = ['U', 'D', 'L', 'R']
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     # min = float("inf")
     starting = []
     for i in range(sizes[0]):
             for j in range(sizes[1]):
                 if labirynth[i][j] == 'S' or labirynth[i][j] == 'B':
                     starting.append((i, j))
+    path = ""
     # active = True
+    # pos = []
     # while(active):
+    # # path = "LLLLLLLDR"
+    #     pos = starting
     #     path = ""
-    #     poses = starting.copy()
-    #     for j in range(80):
-    #         path += random.choice(moves)
-    #     for move in range(len(path)):
-    #         poses = make_moves(labirynth, poses, path[move])
-    #         if len(poses) == 3:
-    #             path = path[:move]
+    #     for i in range(20):
+    #         # (dx, dy) = get_dir(m)
+    #         (dx, dy) = random.choice(directions)
+    #         path += get_move(dx, dy)
+    #         new_pos = []
+    #         # zamienic z formuly bfs-like na formule pod random
+    #         for (x, y) in pos:
+    #             new_x, new_y = x + dx, y + dy
+    #             if labirynth[new_x][new_y] != '#':    
+    #                 if (new_x, new_y) not in new_pos:
+    #                     new_pos.append((new_x, new_y))
+    #             elif (x, y) not in new_pos:
+    #                 new_pos.append((x, y))
+    #         pos = new_pos
+    #         if len(pos) <= 10:
     #             active = False
     #             break
-    # print("done")
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    # starting = pos
     tuple_start = tuple(starting)
-    queue = deque([(tuple_start, "")])
+    queue = deque([(tuple_start, path)])
     visited = {tuple_start}
 
     while(queue):
@@ -117,6 +113,6 @@ if __name__ == "__main__":
     with open("zad_input.txt") as f:
         labirynth = f.readlines()
         path = generate_path(labirynth, (len(labirynth), len(labirynth[0]) - 1))
-        print(path)
+        # print(path)
         with open("zad_output.txt", 'w') as o:
             o.write(path)
