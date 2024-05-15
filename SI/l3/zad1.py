cd039c3adaf543f19f98bdbdc33400ad9e485c23
CzmeleN <344907@uwr.edu.pl>
2024-05-15 19:42:41 +0200

    Small update, still not working

--- SI/l3/zad1.py
+++ SI/l3/zad1.py
@@ -18,6 +18,19 @@ def print_img(img, row_desc, col_desc):
             row_str += " ." if cell == 0 else " #"
         print(row_str)
 
+def aux_create_possibs(no_empty, groups, ones):
+    res_opts = []
+    for p in combinations(range(groups + no_empty), groups):
+        selected = [-1] * (groups + no_empty)
+        ones_idx = 0
+        for val in p:
+            selected[val] = ones_idx
+            ones_idx += 1
+        res_opt = [ones[val] + [-1] if val > -1 else [-1] for val in selected]
+        res_opt = [item for sublist in res_opt for item in sublist][:-1]
+        res_opts.append(res_opt)
+    return res_opts
+
 def create_possibs(values, others_count):
     possibs = []
 
@@ -25,53 +38,67 @@ def create_possibs(values, others_count):
         groups = len(v)
         no_empty = others_count - sum(v) - groups + 1
         ones = [[1] * x for x in v]
-        res = create_possibs(no_empty, groups, ones)
+        res = aux_create_possibs(no_empty, groups, ones)
         possibs.append(res)
 
     return possibs
 
-def resolution(img, row_desc, col_desc):
-    rows_possibs = create_possibs(row_desc, len(img[0]))
-    cols_possibs = create_possibs(col_desc, len(img))
+def resolution(sizes, row_desc, col_desc):
+    img = [[0 for _ in range(sizes[1])] for r in range(sizes[0])]
+    rows_possibs = create_possibs(row_desc, sizes[0])
+    cols_possibs = create_possibs(col_desc, sizes[1])
+    rows_done = [0] * sizes[1]
+    cols_done = [0] * sizes[0]
 
-    while true:
+    ds = 0
+    while True:
+        ds += 1
         lowest_rows = [(i, n, id) for i, n in enumerate([len(i) for i in rows_possibs]) if rows_done[i] == 0]
         lowest_cols = [(i, n, id) for i, n in enumerate([len(i) for i in cols_possibs]) if cols_done[i] == 0]
         lowest = sorted(lowest_rows + lowest_cols, key = lambda element: element[1])
 
         for id1, _, row_id in lowest:
-            if not check_done(row_id, id1):
+            if not (rows_done[id1] if row_id else cols_done[id1]):
                 if row_id:
                     values = rows_possibs[id1]
                 else:
                     values = cols_possibs[id1]
                 same_id = [(n, np.unique(i)[0]) for n, i in enumerate(np.array(values).T) if len(np.unique(i)) == 1]
-                for id2, val in same_id:
+                for id2, v in same_id:
                     if row_id:
                         ri, ci = id1, id2
                     else:
                         ri, ci = id2, id1
                     if img[ri][ci] == 0:
-                        img[ri][ci] = val
+                        img[ri][ci] = v
                         if row_id:
-                            cols_possibs[ci] = [p for p in cols_possibs if p[ri] == val]
+                            cols_possibs[ci] = [p for p in cols_possibs[ci] if p[ri] == v]
                         else:
-                            rows_possibs[ri] = [p for p in rows_possibs if p[ci] == val]
-        if solved:
+                            rows_possibs[ri] = [p for p in rows_possibs[ri] if p[ci] == v]
+                if row_id:
+                    if 0 not in img[id1]:
+                        rows_done[id1] = 1
+                else:
+                    if 0 not in [row[id1] for row in img]:
+                        cols_done[id1] = 1
+        if 0 not in rows_done and 0 not in cols_done:
             return img
+        print_img(img, row_desc, col_desc)
+        if ds == 5:
+            break
 
 def backtrack(sizes, row_desc, col_desc):
     return
 
 if __name__ == "__main__":
     with open("zad_input.txt") as f:
-        row_desc = []
-        col_desc = []
-        sizes = f.readline()
-        sizes = [int(size) for size in sizes.split()]
-        row_desc = [[int(size) for size in f.readline().split()] for i in range(sizes[0])]
-        col_desc = [[int(size) for size in f.readline().split()] for i in range(sizes[1])]
-        painted = almost_walk_sat(sizes, row_desc, col_desc)
+        r_desc = []
+        c_desc = []
+        size = f.readline()
+        size = [int(s) for s in size.split()]
+        r_desc = [[int(s) for s in f.readline().split()] for i in range(size[0])]
+        c_desc = [[int(s) for s in f.readline().split()] for i in range(size[1])]
+        painted = resolution(size, r_desc, c_desc)
         with open("zad_output.txt", 'w') as o:
             for row in painted:
                 row_str = ""
