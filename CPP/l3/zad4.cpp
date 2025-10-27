a1dc8d9bf1b4c57852f76578113c19c1e662bf77
CzmeleN <344907@uwr.edu.pl>
2025-10-27 16:45:56 +0100

    Changed zad4

--- CPP/l3/zad4.cpp
+++ CPP/l3/zad4.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 template<typename Ts, typename Td>
-void my_copy (const Ts &src, Td &dst) {
+void my_copy (Ts &&src, Td &dst) {
     if constexpr (is_pointer_v<Ts>) {
         static_assert(is_convertible_v<remove_pointer_t<Ts>, Td>, "not convertible");
-        dst = *src;
+        dst = std::move(*src);
     } else {
         static_assert(is_convertible_v<Ts, Td>, "not convertible");
-        dst = src;
+        dst = std::move(src);
     }
 }
 
@@ -23,11 +23,23 @@ int main() {
 
     my_copy(x, y);
     x = 2;
-    cout << "(expected: 1) " << y << endl;
+    cout << " (expected: 1) " << y << endl;
 
     my_copy(&x, y);
     x = 3;
     cout << "(expected: 2) " << y << endl;
+
+    string one = "test";
+    string two = "";
+
+    my_copy(one, two);
+    cout << one << " two: " << two << endl;
+
+    one = "one";
+    two = "";
+
+    my_copy(&one, two);
+    cout << one << " two: " << two << endl;
 
     // nie skompiluje się
     // test z;
