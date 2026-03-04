d15ce412b745ceedf3c9756fb328465f8916be58
CzmeleN <344907@uwr.edu.pl>
2026-03-04 16:09:03 +0100

    Added z1 from cpp

---
+++ C++/zad1.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+using ll = long long;
+
+bool is_prime(ll x) {
+    if (x <= 1) return false;
+
+    for (ll i = 2; i * i <= x; ++i) {
+        if (x % i == 0) return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        ll x = std::stoll(argv[i]);
+
+        if (is_prime(x)) std::cout << x << ' ';
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
